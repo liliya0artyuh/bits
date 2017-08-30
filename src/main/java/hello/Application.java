0facf1a32a5afc8d38300f3f8affeac328f59d5b
liliya0artyuh <liliya0artyukh@gmail.com>
2017-08-29 21:53:30 -0400

    updated code for mongodb connection

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,15 @@
 package hello;
 
+import java.rmi.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.DB;
+import com.mongodb.MongoException;
+import com.mongodb.MongoURI;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +17,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 }
